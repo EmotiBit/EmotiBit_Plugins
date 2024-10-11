b42e5e1c950f92b6a7d8f84d5cb089d3b599f313
ThomasNels <thomasnelson2005@gmail.com>
2024-10-11 10:52:24 -0700

    feat v1.0.8: intial DLL test

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,27 +1,7 @@
-#include<cmath>
-#ifdef PYBIND11_ENABLED
-#include<pybind11/pybind11.h>
-#endif
-
-class Rounder {
-    public:
-        int round(double& num) {
-            return std::round(num);
-        }
-};
+#include "alg01.h"
 
 int main() {
     Rounder r;
     double num = 5.223;
     r.round(num);
 }
-
-#ifdef PYBIND11_ENABLED
-namespace py = pybind11;
-
-PYBIND11_MODULE(rounder, m) {
-    py::class_<Rounder>(m, "Rounder")
-        .def(py::init<>())
-        .def("round", &Rounder::round);
-}
-#endif
